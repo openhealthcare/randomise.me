aee798680b96cf72c71ac96bbcf3bcb9fac42268
David Miller <David>
2013-04-15 15:51:37 +0100

    Update Action of RM trial creation. refs #33

---
+++ rm/test/trials/test_trial_views.py
@@ -0,0 +1,26 @@
+"""
+Unittests for Trial views
+"""
+from django import test
+from django.test import Client
+from lxml import html
+
+class CreateRMTrialTestCase(test.TestCase):
+
+    def test_form_action(self):
+        "Regression test #33"
+        c = Client()
+        with self.settings(BASICAUTH=False):
+            resp = c.get('/trials/rm/new')
+            self.assertEqual(200, resp.status_code)
+            dom = html.fromstring(resp.content)
+            form = dom.cssselect('form')[0]
+            self.assertEqual('', form.get('action'))
+
+
+class JoinTrialTestCase(test.TestCase):
+    "Joining a trial"
+
+    def test_get(self):
+        "Joins Trial"
+        pass
