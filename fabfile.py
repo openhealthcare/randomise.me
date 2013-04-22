3c70805b0a46742e78952d5bc9555a2fb99c9dc0
David Miller <David>
2013-04-22 11:18:32 +0100

    Update to use Supervisord

--- fabfile.py
+++ fabfile.py
@@ -9,6 +9,7 @@ PROJ_DIR = '/usr/local/ohc/randomise.me'
 VENV_BIN = '/home/ohc/.virtualenvs/rm/bin/{0}'
 venv_bin = lambda x: VENV_BIN.format(x)
 VENV_PY = venv_bin('python')
+VENV_CTL = venv_bin('supervisorctl')
 
 def manage(what):
     """
@@ -20,6 +21,18 @@ def manage(what):
     with cd(PROJ_DIR):
         run('{0} manage.py {1}'.format(VENV_PY, what))
 
+def supervisorctl(what):
+    """
+    Run a supervisorctl command
+
+    Arguments:
+    - `what`: the command to run
+
+    Return: None
+    Exceptions: None
+    """
+    run('{ctl} {what}'.format(ctl=VENV_CTL, what=what))
+
 def migrate():
     """
     Update the database
@@ -30,18 +43,23 @@ def stop():
     """
     Stop the application in production
     """
-    with cd(PROJ_DIR):
-        run('pkill gunicorn')
-        run("ps auxww | grep celery| awk '{print $2}'| xargs kill -9")
+    run("kill -9 `cat /usr/local/ohc/var/run/supervisord.pid`")
+#    supervisorctl('stop')
+    # with cd(PROJ_DIR):
+        # run('pkill gunicorn || echo "Gunicorn not running"')
+        # run("ps auxww | grep celery | grep -v grep| awk '{print $2}'| xargs kill -9 || echo 'Celery not running'")
+
 
 def start():
     """
     Start the application in production.
     """
     with cd(PROJ_DIR):
-        run('{0} -D -c gunicorn_conf.py'.format(venv_bin('gunicorn_django')))
-        manage('celery worker')
-        manage('celerybeat -S djcelery.schedulers.DatabaseScheduler')
+        supervisord = venv_bin('supervisord')
+        run('{supervisord} -c etc/production.conf'.format(supervisord=supervisord))
+        # run('{0} -D -c gunicorn_conf.py'.format(venv_bin('gunicorn_django')))
+        # manage('celery worker --scheduler=djcelery.schedulers.DatabaseScheduler -B')
+#        manage('celerybeat -S djcelery.schedulers.DatabaseScheduler')
 
 @hosts(web)
 def deploy():
@@ -52,8 +70,7 @@ def deploy():
         run('git pull origin master') #not ssh - key stuff
         run('{0} install -r requirements.txt'.format(venv_bin('pip')))
         migrate()
-        stop()
-        start()
+        restart()
 
 
 @hosts(web)
@@ -61,5 +78,5 @@ def restart():
     """
     Restart the application
     """
-    stop()
-    start()
+    supervisorctl('reread')
+    supervisorctl('restart all')
