c1e569de2849fb7946f05142be41cbba15e34e2b
David Miller <david@deadpansincerity.com>
2013-05-07 15:40:58 +0100

    Don't assume authentication for trial detail. closes #60

--- rm/test/trials/test_trial_views.py
+++ rm/test/trials/test_trial_views.py
@@ -2,18 +2,25 @@
 Unittests for Trial views
 """
 from django import test
-from django.test import Client
 from lxml import html
 
 from rm.trials import views
+
+class DetailTestCase(test.TestCase):
+
+    def test_200_not_logged_in(self):
+       " Assert status Regression #60"
+       with self.settings(BASICAUTH=False):
+           response = self.client.get('/trials/rm/1')
+           self.assertEqual(200, response.status_code)
+
 
 class CreateRMTrialTestCase(test.TestCase):
 
     def test_form_action(self):
         "Regression test #33"
-        c = Client()
         with self.settings(BASICAUTH=False):
-            resp = c.get('/trials/rm/new')
+            resp = self.client.get('/trials/rm/new')
             self.assertEqual(200, resp.status_code)
             dom = html.fromstring(resp.content)
             form = dom.cssselect('form')[0]
