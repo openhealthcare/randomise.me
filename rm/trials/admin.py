819fb918e9a1738f58dc4abb8c7680a7b900462b
David Miller <david@deadpansincerity.com>
2013-10-04 10:43:34 +0100

    Add invitations to admin

--- rm/trials/admin.py
+++ rm/trials/admin.py
@@ -3,10 +3,12 @@ Register Trials with the admin site.
 """
 from django.contrib import admin
 
-from rm.trials.models import (Trial, Report, Participant, Variable, TutorialExample)
+from rm.trials.models import (Trial, Report, Participant, Variable,
+                              TutorialExample, Invitation)
 
 class VariableInline(admin.StackedInline):
     model = Variable
+
 
 class TrialAdmin(admin.ModelAdmin):
     """
@@ -19,15 +21,18 @@ class TrialAdmin(admin.ModelAdmin):
     list_editable = ['featured','hide']
     inlines = [VariableInline]
 
+
 class VariableAdmin(admin.ModelAdmin):
     list_display = ('trial', 'question', 'style')
     list_filter = ['trial', 'style']
+
 
 class ParticipantAdmin(admin.ModelAdmin):
     """
     Manual admin of participants
     """
     list_filter = ('user', 'trial')
+
 
 class ReportAdmin(admin.ModelAdmin):
     """
@@ -36,12 +41,18 @@ class ReportAdmin(admin.ModelAdmin):
     list_filter = ('trial',)
     list_display = ['trial', 'date', 'group', 'get_value']
 
+
 class TutorialAdmin(admin.ModelAdmin):
     list_display  = ['name', 'question', 'measure_style', 'measure_question', 'group_a', 'group_b']
     list_editable = ['question', 'measure_style', 'measure_question', 'group_a', 'group_b']
+
+
+class InvitationAdmin(admin.ModelAdmin):
+    model = Invitation
 
 admin.site.register(Participant, ParticipantAdmin)
 admin.site.register(Trial, TrialAdmin)
 admin.site.register(Report, ReportAdmin)
 admin.site.register(Variable, VariableAdmin)
 admin.site.register(TutorialExample, TutorialAdmin)
+admin.site.register(Invitation, InvitationAdmin)
